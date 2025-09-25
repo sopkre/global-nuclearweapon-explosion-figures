b4958c0a7479e8cc35dfc5b8edb0507442dcdf63
SK <mail@k-mar.de>
2025-09-25 16:11:11 +0200

    Put many helpers functions to helpers lib

--- global-nuclearweapon-explosion-figures/helpers.py
+++ global-nuclearweapon-explosion-figures/helpers.py
@@ -4,6 +4,7 @@ Code snippet to jelp plotting, i/o and converting to strings.
 
 import pickle
 import re
+import numpy as np
 
 COLORS_ = {
     "FR" : '#e41a1c',
@@ -194,6 +195,7 @@ def save_pkl(something, outfilename):
     pickle.dump(something, output)
     output.close()
 
+
 def sort_list_by_score(ll, score):
     """Sorts list ll according to sorting of score list (i.e. ["x","c","d"] and [2,1,3] -> ["c","x","d"])
     ---------
@@ -206,6 +208,7 @@ def sort_list_by_score(ll, score):
     score, ll = zip(*sorted(zip(score, ll)))
     return ll[::-1]
 
+
 def get_part_before_hyphen(s):
     """Deletes characters after (and including) hyphen
     ---------
@@ -215,6 +218,7 @@ def get_part_before_hyphen(s):
     if type(s) is str and s.find("-") > -1:
         s = s[:s.find("-")]
     return s
+
 
 def get_part_after_hyphen(s):
     """Deletes characters after (and including) hyphen
@@ -227,3 +231,110 @@ def get_part_after_hyphen(s):
     else: 
         s = None
     return s
+
+YIELD_BINS_ = [0.01, 1, 10, 50, 100, 1000, 10000]
+
+
+def format_yield_and_add_unit(bins=YIELD_BINS_):
+    """Formats yield values (e.g. 1.0 -> 1) and adds units (e.g. kT, MT)
+    Parameters
+    ---------
+        bins : list of float 
+            bins to make formated list from
+    """
+    bins_s = []
+    for b in bins:
+        if b < 1:
+            bins_s += [f"{b} kT"]
+        elif b < 1000:
+            bins_s += [f"{b:.0f} kT"]
+        else:
+            bins_s += [f"{b/1000:.0f} MT"]
+    return bins_s
+
+
+def get_yield_range_str(y, bins=YIELD_BINS_):
+    """Formats bins to label list for yield ranges, e.g. ["< 0.1 kT", "0.1kT - 20 MT"] 
+    Parameters
+    ---------
+        bins : list of float 
+            bins to make formated list from
+    """
+    i = 0
+    while i < len(bins) and y > bins[i]:
+        i += 1  
+    bins_s = format_yield_and_add_unit(bins)
+    if (i == 0):
+        s = f"< {bins_s[i]}"
+    elif (i < len(bins)):
+        s = f"{bins_s[i-1]} - {bins_s[i]}"
+    else:
+        s = f"> {bins_s[-1]}"
+    if np.isnan(y): 
+        s = "n/a"
+    return s
+
+
+def get_explosion_type(typestr): 
+    """Get type (e.g. "underground") from explosion type (part before hyphen)
+    Parameters
+    ---------
+        typestr : str
+            TYPE string
+    """
+    return get_part_before_hyphen(typestr)
+
+
+def get_delivery(typestr): 
+    """Get delivery from explosion type (part after hyphen)
+    Parameters
+    ---------
+        typestr : str
+            TYPE string
+    """
+    return get_part_after_hyphen(typestr)
+
+def get_explosion_purpose(p):
+    """Formats explosion purpose (combine into "other" cat, remove "?")
+    Parameters
+    ---------
+        p : str
+            purpose string
+    """
+    p = get_part_before_hyphen(p)
+    other = ["JV", "C", "WR/PR", "VU", "ST", "ME"]
+
+    if type(p) is str:
+        p = p.replace("?", "")
+
+    if p is None or (type(p) is float and np.isnan(p)): 
+        p = "n/a"
+
+    if p in other:
+        p = "other"
+
+    return p
+
+
+def make_yield_color_dict(bins=YIELD_BINS_):
+    """Makes color dictionary for yield bins (increasingly darker red). 
+    Parameters
+    ---------
+        bins : list of float 
+            bins to convert to colour range
+    """
+    bins = format_yield_and_add_unit()
+    color_dict = {}
+
+    for i in range(len(bins)+1):
+        if (i == 0):
+            key = f"< {bins[i]}"
+        elif (i < len(bins)):
+            key = f"{bins[i-1]} - {bins[i]}"
+        else:
+            key = f"> {bins[-1]}"
+        color_dict[key] = f'rgb({(1-i/len(bins))*255}, 0, 0)'
+    
+    color_dict["n/a"] = 'rgb(240, 240, 240)'
+
+    return color_dict
--- global-nuclearweapon-explosion-figures/plot_pies.py
+++ global-nuclearweapon-explosion-figures/plot_pies.py
@@ -16,109 +16,6 @@ import helpers
 
 YIELD_BINS_ = [0.01, 1, 10, 50, 100, 1000, 10000]
 
-def get_explosion_type(typestr): 
-    """Get type (e.g. "underground") from explosion type (part before hyphen)
-    Parameters
-    ---------
-        typestr : str
-            purpose string
-    """
-    return helpers.get_part_before_hyphen(typestr)
-
-def get_delivery(typestr): 
-    """Get delivery from explosion type (part after hyphen)
-    Parameters
-    ---------
-        typestr : str
-            purpose string
-    """
-    return helpers.get_part_after_hyphen(typestr)
-
-def get_explosion_purpose(p):
-    """Formats explosion purpose (combine into "other" cat, remove "?")
-    Parameters
-    ---------
-        p : str
-            purpose string
-    """
-    p = helpers.get_part_before_hyphen(p)
-    other = ["JV", "C", "WR/PR", "VU", "ST", "ME"]
-
-    if type(p) is str:
-        p = p.replace("?", "")
-
-    if p is None or (type(p) is float and np.isnan(p)): 
-        p = "n/a"
-
-    if p in other:
-        p = "other"
-
-    return p
-
-def format_and_add_unit(bins=YIELD_BINS_):
-    """Formats yield values (e.g. 1.0 -> 1) and adds units (e.g. kT, MT)
-    Parameters
-    ---------
-        bins : list of float 
-            bins to make formated list from
-    """
-    bins_s = []
-    for b in bins:
-        if b < 1:
-            bins_s += [f"{b} kT"]
-        elif b < 1000:
-            bins_s += [f"{b:.0f} kT"]
-        else:
-            bins_s += [f"{b/1000:.0f} MT"]
-    return bins_s
-
-def get_yield_range_str(y, bins=YIELD_BINS_):
-    """Formats bins to label list for yield ranges, e.g. ["< 0.1 kT", "0.1kT - 20 MT"] 
-    Parameters
-    ---------
-        bins : list of float 
-            bins to make formated list from
-    """
-    i = 0
-    while i < len(bins) and y > bins[i]:
-        i += 1  
-    bins_s = format_and_add_unit(bins)
-    if (i == 0):
-        s = f"< {bins_s[i]}"
-    elif (i < len(bins)):
-        s = f"{bins_s[i-1]} - {bins_s[i]}"
-    else:
-        s = f"> {bins_s[-1]}"
-    if np.isnan(y): 
-        s = "n/a"
-    return s
-
-def make_yield_color_dict(bins=YIELD_BINS_):
-    """Makes color dictionary for yield bins (increasingly darker red). 
-    Parameters
-    ---------
-        bins : list of float 
-            bins to convert to colour range
-    """
-    bins = format_and_add_unit()
-    color_dict = {}
-
-    for i in range(len(bins)+1):
-        if (i == 0):
-            key = f"< {bins[i]}"
-        elif (i < len(bins)):
-            key = f"{bins[i-1]} - {bins[i]}"
-        else:
-            key = f"> {bins[-1]}"
-        color_dict[key] = f'rgb({(1-i/len(bins))*255}, 0, 0)'
-    
-    color_dict["n/a"] = 'rgb(240, 240, 240)'
-
-    return color_dict
-
-
-
-
 def make_pie(df, slice="STATE"):
     """Plot pie.
     Parameters
@@ -149,7 +46,7 @@ def make_pie(df, slice="STATE"):
         colors = [helpers.TYPECOLORS_[x] for x in labels] 
         labels = [helpers.TYPESLABEL_[x] for x in labels]
     elif slice =="YIELD_CAT":
-        color_dict = make_yield_color_dict()
+        color_dict = helpers.make_yield_color_dict()
         labels = list(color_dict.keys())[::-1]
         values = [len(df[df[slice]==x]) for x in labels]
         colors = [color_dict[x] for x in labels]
@@ -201,9 +98,9 @@ def main(infilename, outfilename):
     ### -----------------
     df = helpers.load_pkl(infilename)
 
-    df["TYPE_SHORT"] = df["TYPE"].apply(lambda x: get_explosion_type(x))
-    df["PUR_SHORT"] = df["PUR"].apply(lambda x: get_explosion_purpose(x))
-    df["YIELD_CAT"] = df["YIELD"].apply(lambda x: get_yield_range_str(x))
+    df["TYPE_SHORT"] = df["TYPE"].apply(lambda x: helpers.get_explosion_type(x))
+    df["PUR_SHORT"] = df["PUR"].apply(lambda x: helpers.get_explosion_purpose(x))
+    df["YIELD_CAT"] = df["YIELD"].apply(lambda x: helpers.get_yield_range_str(x))
 
     ### Make figure
     ### -----------
